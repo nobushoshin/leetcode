b41f96601182d9d4837c2f92956496c2f4fdfded
nobushoshin <102891179+nobushoshin@users.noreply.github.com>
2023-12-29 11:22:57 +0900

    Add files via upload

---
+++ 1232_CheckIfItIsaStraightLine.py
@@ -0,0 +1,24 @@
+
+
+class Solution:
+    def checkStraightLine(self, coordinates: list[list[int]]) -> bool:
+
+        x1, y1 = coordinates[0][0], coordinates[0][1]
+        x2, y2 = coordinates[1][0], coordinates[1][1]
+
+        if x1 == x2:
+            for coordinate in coordinates:
+                if coordinate[0] != x1:
+                    return False                    
+            return True
+        else:
+            a = (y2 - y1) / (x2 - x1)
+            b = y1 - a * x1
+            for coordinate in coordinates:
+                if (a * coordinate[0] + b) != coordinate[1]:
+                    return False
+            return True
+
+coordinates = [[0,0],[0,1],[0,-1]]
+solution = Solution()
+print(solution.checkStraightLine(coordinates))
